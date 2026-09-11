cbb2d5793d199fa416a58ec64e1eacf44a003767
Virpitz <dmitri.prosvirov@mail.ru>
2026-09-11 19:32:07 +0300

    learn dart functions

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -1,3 +1,24 @@
+String repeat(String text, [int times = 2]) {
+  String result = '';
+  for (int i = 0; i < times; i++) {
+    result += text;
+  }
+  return result;
+}
+
+String greet(String name) {
+    return 'Привет, $name!';
+  }
+
+int square(int x) => x * x;
+double half(double x) => x / 2;
+
+void describePet({required String name, String species = 'кот', int age = 0}) {
+  print('$name - $species, возраст $age');
+}
+
+
+
 void main() {
   String name = 'Дмитрий';
   int age = 18;
@@ -50,4 +71,26 @@ void main() {
   for (var fruit in fruits2) {
     print(fruit);
   }
+
+  print(greet('Дмитрий'));
+  print(square(23));
+  print(half(2.45));
+
+  describePet(name: 'Барсик', age: 3);
+  describePet(name: 'Бобик', species: 'Пёс');
+
+print(repeat('ха'));
+print(repeat('ха', 3));
+
+List<int> numbers = [3, 2, 1, 4, 1, 5 , 9];
+numbers.sort((a, b) => b - a);
+print(numbers);
+
+List<String> names = ['Владимир', 'Иосиф', 'Феликс'];
+
+List<String> upper = names.map((names) => name.toUpperCase()).toList();
+print(upper);
+
+List<String> longNames = names.where((name) => name.length > 4).toList();
+print(longNames);
 }
